a128f200ed71f07a0e2647853798ca74478dad13
Tomoaki Hayasaka <hayasaka@malmodeler.com>
2024-09-30 02:24:55 +0900

    feat:  implement BurstSS spawner strategy.

--- Assets/00_Spinometer/Screens/WhackGame/SpawnerStrategy/BurstSS.cs
+++ Assets/00_Spinometer/Screens/WhackGame/SpawnerStrategy/BurstSS.cs
@@ -1,22 +1,55 @@
 ﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace GetBack.Spinometer.Screens.WhackGame.SpawnerStrategy
 {
   public class BurstSS : ISpawnerStrategy
   {
-    private MoleManager _manager;
+    private MoleManager _moleManager;
+    private WhackGame _whackGame;
+    private double _nextBurstStartTime;
+    private int _burstCountLeft;
 
-    public BurstSS(MoleManager manager)
+    private CancellationTokenSource _cts;
+
+    public BurstSS(MoleManager moleManager)
     {
-      _manager = manager;
+      _moleManager = moleManager;
+      _whackGame = _moleManager.whackGame;
+      _nextBurstStartTime = Time.timeAsDouble;
+      _burstCountLeft = 0;
+      _cts = new();
     }
 
     void IDisposable.Dispose()
     {
+      _cts.Cancel();
+      _cts.Dispose();
+      _cts = null;
     }
 
     void ISpawnerStrategy.NextTick(double currentTime, float deltaTime)
     {
+      if (currentTime >= _nextBurstStartTime) {
+        StartBurst(currentTime);
+      }
+    }
+
+    private async void StartBurst(double currentTime)
+    {
+      _burstCountLeft = 5;
+      _nextBurstStartTime = currentTime + 2.0;
+
+      for (int i = 0; i < 5; i++) {
+        _moleManager.Spawn(currentTime);
+        var isCanceled = await UniTask.Delay(30, cancellationToken: _cts.Token).SuppressCancellationThrow();
+        if (isCanceled) {
+          Debug.Log("canceled");
+          return;
+        }
+      }
     }
   }
 }
