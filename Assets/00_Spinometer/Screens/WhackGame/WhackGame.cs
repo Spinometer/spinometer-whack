d08e65242900a952d9b818d441f88c513404cbc2
Tomoaki Hayasaka <hayasaka@malmodeler.com>
2024-10-09 15:15:08 +0900

    feat:  use the scores based on absolute angle to calculate game score.

--- Assets/00_Spinometer/Screens/WhackGame/WhackGame.cs
+++ Assets/00_Spinometer/Screens/WhackGame/WhackGame.cs
@@ -144,7 +144,7 @@ namespace GetBack.Spinometer.Screens.WhackGame
 
     private void UpdateAlignmentScore()
     {
-      var scores = _tracker.spinalAlignmentScore.relativeAngleScores;
+      var scores = _tracker.spinalAlignmentScore.absoluteAngleScores;
       var average = scores.Count == 0 ? 0f : scores.Average(kv => kv.Value);
       _alignmentScore += average * Time.deltaTime;
       _whackGameUiDataSource.alignmentScore = _alignmentScore;
--- Assets/00_Spinometer/Screens/WhackResult/ScoreGraph.cs
+++ Assets/00_Spinometer/Screens/WhackResult/ScoreGraph.cs
@@ -43,11 +43,14 @@ namespace GetBack.Spinometer.Screens.WhackResult
       var m1 = new Matrix4x4();
       var t0 = gameStateLog.timeRemainingMax;
       var t1 = gameStateLog.timeRemainingMin;
+      var score0 = 0f;
+      var score1 = 1f;
       float localCenterX = (t1 - t0) * 0.5f;
-      m0.SetTRS(new Vector3(localCenterX, 0f, 0f), Quaternion.identity, Vector3.one);
+      float localCenterY = (score1 - score0) * -0.5f;
+      m0.SetTRS(new Vector3(localCenterX, localCenterY, 0f), Quaternion.identity, Vector3.one);
       Vector3 localToGlobalScale =
         new Vector3(width / (t1 - t0),
-                    height / 2f,
+                    height / (score1 - score0),
                     1f);
       m1.SetTRS(center, Quaternion.identity, localToGlobalScale);
       return m1 * m0;
@@ -87,27 +90,26 @@ namespace GetBack.Spinometer.Screens.WhackResult
           }
           if (false) {
             // plot individual scores, for debug
-            var ids = replayBuffer.entries[0].spinalAlignmentScore.relativeAngleScores.Keys;
+            var ids = replayBuffer.entries[0].spinalAlignmentScore.absoluteAngleScores.Keys;
             var hue = 0f;
             foreach (var id in ids) {
               Debug.Log($"id = {id}, hue = {hue}");
-              SpinalAlignment.RelativeAngleId[] group = { id };
+              SpinalAlignment.AbsoluteAngleId[] group = { id };
               RenderGroup(group, Color.HSVToRGB(hue, 1f, 1f));
               hue += 0.16f;
             }
           }
           if (true) {
-            SpinalAlignment.RelativeAngleId[] group0 = new[] {
-              SpinalAlignment.RelativeAngleId.C2_C7_vert_new,
-              SpinalAlignment.RelativeAngleId.C7_T3_vert_new,
+            SpinalAlignment.AbsoluteAngleId[] group0 = new[] {
+              SpinalAlignment.AbsoluteAngleId.C2_C7,
+              SpinalAlignment.AbsoluteAngleId.C7_T3
             };
-            SpinalAlignment.RelativeAngleId[] group1 = new[] {
-              SpinalAlignment.RelativeAngleId.C7_T3_T8,
-              SpinalAlignment.RelativeAngleId.T3_T8_T12,
+            SpinalAlignment.AbsoluteAngleId[] group1 = new[] {
+              SpinalAlignment.AbsoluteAngleId.T3_T8,
+              SpinalAlignment.AbsoluteAngleId.T8_T12,
             };
-            SpinalAlignment.RelativeAngleId[] group2 = new[] {
-              SpinalAlignment.RelativeAngleId.T8_T12_L3,
-              SpinalAlignment.RelativeAngleId.T12_L3_S,
+            SpinalAlignment.AbsoluteAngleId[] group2 = new[] {
+              SpinalAlignment.AbsoluteAngleId.L3_S,
             };
 
             RenderGroup(group0, new Color(1f, 0.3f, 0.3f));
@@ -118,21 +120,21 @@ namespace GetBack.Spinometer.Screens.WhackResult
       }
     }
 
-    private void RenderGroup(SpinalAlignment.RelativeAngleId[] group0, Color color)
+    private void RenderGroup(SpinalAlignment.AbsoluteAngleId[] group0, Color color)
     {
 
-      float Or0(Dictionary<SpinalAlignment.RelativeAngleId, float> scores, SpinalAlignment.RelativeAngleId id)
+      float Or0(Dictionary<SpinalAlignment.AbsoluteAngleId, float> scores, SpinalAlignment.AbsoluteAngleId id)
       {
         return scores.ContainsKey(id) ? scores[id] : 0f;
       }
 
       using (Draw.ingame.WithColor(color)) {
         var e0 = replayBuffer.entries[0];
-        var scores0 = e0.spinalAlignmentScore.relativeAngleScores;
+        var scores0 = e0.spinalAlignmentScore.absoluteAngleScores;
         var s0 = group0.Select(id => Or0(scores0, id)).Average();
         for (int i = 1; i < replayBuffer.entries.Count; i++) {
           var e1 = replayBuffer.entries[i];
-          var scores1 = e1.spinalAlignmentScore.relativeAngleScores;
+          var scores1 = e1.spinalAlignmentScore.absoluteAngleScores;
           var s1 = group0.Select(id => Or0(scores1, id)).Average();
           Draw.ingame.Circle(new Vector3(e1.timeRemaining, s1, 0f), Vector3.back, 0.01f);
           Draw.ingame.Line(new Vector3(e0.timeRemaining, s0, 0f), new Vector3(e1.timeRemaining, s1, 0f));
